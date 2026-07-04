c9c9cd1e66ee9824481b8cb7311e790bf126f8e2
insop1 <dlos9383@gmail.com>
2026-07-04 02:55:55 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+#dotfiles
+
+Fedora 44 + Hyprland.
+
+#important
+
+The waybar config uses [insop-media](https://github.com/Insop1/insop-media), you can either remove the custom module or build it.
+
+\`\`\`bash
+git clone https://github.com/Insop1/insop-media ~/.config/waybar/scripts/insop-media
+cd ~/.config/waybar/scripts/insop-media
+cargo build --release
+\`\`\`
+
---
+++ hypr/scripts/restore-gtk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp $HOME/.config/hypr/insop-gtk-color-override.css $HOME/.config/gtk-3.0/colors.css
+cp $HOME/.config/hypr/insop-gtk-color-override.css $HOME/.config/gtk-4.0/colors.css
+
+cp $HOME/.config/hypr/insop-gtk-setting-override.ini $HOME/.config/gtk-3.0/settings.ini
+cp $HOME/.config/hypr/insop-gtk-setting-override.ini $HOME/.config/gtk-4.0/settings.ini
