64ddcd202f570b1672ad774162b1aae675f0c584
insop1 <dlos9383@gmail.com>
2026-07-04 03:00:02 -0400

    Remove unnecessary files

--- .config/hypr/scripts/restore-gtk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp $HOME/.config/hypr/insop-gtk-color-override.css $HOME/.config/gtk-3.0/colors.css
-cp $HOME/.config/hypr/insop-gtk-color-override.css $HOME/.config/gtk-4.0/colors.css
-
-cp $HOME/.config/hypr/insop-gtk-setting-override.ini $HOME/.config/gtk-3.0/settings.ini
-cp $HOME/.config/hypr/insop-gtk-setting-override.ini $HOME/.config/gtk-4.0/settings.ini
